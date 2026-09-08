ae4cabc6a792dfe0cf3f808a822976c4f7a77cf0
guiamorim07 <guiam470232@gmail.com>
2026-09-07 21:42:07 -0300

    troca do sqlite para postgresql no settings.py

--- laudos_config/settings.py
+++ laudos_config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'laudos_config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '261007guiAMO',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
